0b9caa475f537f0f29a0117d5d6818396026f43c
Rainie Chi <lemoncc1222@gmail.com>
2023-11-20 19:49:53 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Navbar/>
+        <Switch>
+          
+        </Switch>
+        </Router>
     </div>
   );
 }
