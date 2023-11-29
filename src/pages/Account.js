55f2297370422f08a18a808a1afc68433b56d1fe
Rainie Chi <lemoncc1222@gmail.com>
2023-11-29 11:44:51 -0800

    updated History table and completed Account backend connection

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,21 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import SideMenu from '../components/SideMenu';
 import '../styles/Account.css';
 
 function Account() {
-
-  //takes json from backend to display valuues
-  
-  
-  const [firstName, setFirstName] = useState('Remington');
-  const [lastName, setLastName] = useState('Riley');
-  const [email, setEmail] = useState('rem@rem.org');
-  const [password, setPassword] = useState('********');
-  const [cellphone, setCellphone] = useState('123-456-7890');
-  const [location, setLocation] = useState('City, Country');
-  const [Lat, setLat] = useState('47.253078');
-  const [Lon, setLon] = useState('-122.441528');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cellphone, setCellphone] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
 
   const handleSave = () => {
     const userData = {
@@ -24,103 +19,121 @@ function Account() {
       email,
       password,
       cellphone,
-      location,
-      latitude: Lat,
-      longitude: Lon,
+      latitude,
+      longitude,
     };
 
-    const jsonData = JSON.stringify(userData)
-    //send to back end
+    const jsonData = JSON.stringify(userData);
+    // send to the backend
 
-    
-    console.log(jsonData)
+    console.log(jsonData);
   };
 
+  useEffect(() => {
+
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/currentUser');
+        if (!response.ok) {
+          console.error('Failed to fetch current user data');
+          return;
+        }
+        const userData = await response.json();
+        console.log(userData);
+        setFirstName(userData.Fname);
+        setLastName(userData.Lname);
+        setEmail(userData.email);
+        setPassword(userData.password); 
+        setCellphone(userData.cellphone);
+        setLatitude(userData.LAT);
+        setLongitude(userData.LON);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
 
   return (
     <div className="Account">
       <Navbar />
       <SideMenu />
-<form className="account-form">
-  <div className="form-group">
-    <label htmlFor="firstName">First Name:</label>
-    <input
-      type="text"
-      id="firstName"
-      value={firstName}
-      onChange={(e) => setFirstName(e.target.value)}
-    />
-  </div>
+      <form className="account-form">
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
 
-  <div className="form-group">
-    <label htmlFor="lastName">Last Name:</label>
-    <input
-      type="text"
-      id="lastName"
-      value={lastName}
-      onChange={(e) => setLastName(e.target.value)}
-    />
-  </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
 
-  <div className="form-group">
-    <label htmlFor="email">Email:</label>
-    <input
-      type="email"
-      id="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-    />
-  </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-  <div className="form-group">
-    <label htmlFor="password">Password:</label>
-    <input
-      type="password"
-      id="password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-    />
-  </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-  <div className="form-group">
-    <label htmlFor="cellphone">Cellphone:</label>
-    <input
-      type="tel"
-      id="cellphone"
-      value={cellphone}
-      onChange={(e) => setCellphone(e.target.value)}
-    />
-  </div>
+        <div className="form-group">
+          <label htmlFor="cellphone">Cellphone:</label>
+          <input
+            type="tel"
+            id="cellphone"
+            value={cellphone}
+            onChange={(e) => setCellphone(e.target.value)}
+          />
+        </div>
 
-  <div className="form-group">
-    <label htmlFor="location">Location:</label>
-    <input
-      type="text"
-      id="location"
-      value={location}
-      onChange={(e) => setLocation(e.target.value)}
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="latitude">Latitude:</label>
-    <input
-      type="text"
-      id="latitude"
-      value={Lat}
-      onChange={(e) => setLat(e.target.value)}
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="longitude">Longitude:</label>
-    <input
-      type="text"
-      id="longitude"
-      value={Lon}
-      onChange={(e) => setLon(e.target.value)}
-    />
-  </div>
-  <button type="button" onClick={handleSave}>Save</button>
-</form>
+        <div className="form-group">
+          <label htmlFor="latitude">Latitude:</label>
+          <input
+            type="text"
+            id="latitude"
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="longitude">Longitude:</label>
+          <input
+            type="text"
+            id="longitude"
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+        </div>
+
+        <button type="button" onClick={handleSave}>
+          Save
+        </button>
+      </form>
     </div>
   );
 }
--- src/pages/History.js
+++ src/pages/History.js
@@ -7,8 +7,8 @@ function History() {
 
     //mock data for testing
   const historyData = [
-    { date: '2023-01-01', location: 'City A', ozone: 10, pm25: 5, pm10: 15 },
-    { date: '2023-02-01', location: 'City B', ozone: 15, pm25: 8, pm10: 20 },
+    { date: '2023-01-01', lat: 123, lon:123, ozone: 10, pm25: 5, pm10: 15, co:2, so2:2, no2:3 },
+    { date: '2023-01-01', lat: 123, lon:123, ozone: 10, pm25: 5, pm10: 15, co:2, so2:2, no2:3 },
   ];
 
   //takes actual JSON from backend
@@ -24,20 +24,28 @@ function History() {
           <thead>
             <tr>
               <th>Date</th>
-              <th>Location</th>
+              <th>Lat</th>
+              <th>Lon</th>
               <th>Ozone</th>
               <th>PM2.5</th>
               <th>PM10</th>
+              <th>CO</th>
+              <th>SO2</th>
+              <th>NO2</th>
             </tr>
           </thead>
           <tbody>
             {historyData.map((entry, index) => (
               <tr key={index}>
                 <td>{entry.date}</td>
-                <td>{entry.location}</td>
+                <td>{entry.lat}</td>
+                <td>{entry.lon}</td>
                 <td>{entry.ozone}</td>
                 <td>{entry.pm25}</td>
                 <td>{entry.pm10}</td>
+                <td>{entry.co}</td>
+                <td>{entry.so2}</td>
+                <td>{entry.no2}</td>
               </tr>
             ))}
           </tbody>
